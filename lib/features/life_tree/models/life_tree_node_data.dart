f21cdfc76bed650f71b2404eb8d607dd514f0ef6
Matthias Lindner <two4god@gmail.com>
2026-03-26 06:33:47 +0100

    fix: remove confusing type system, stabilize layout with fixed node sizes and implement note overlay fixe: #16

--- lib/features/life_tree/models/life_tree_node_data.dart
+++ lib/features/life_tree/models/life_tree_node_data.dart
@@ -1,37 +1,28 @@
 import 'package:equatable/equatable.dart';
 
-enum LifeTreeNodeType {
-  branch,
-  leaf
-}
-
 class LifeTreeNodeData extends Equatable {
   final String id;
   final String text;
   final String note;
   final String? parentId;
-  final LifeTreeNodeType type;
 
   const LifeTreeNodeData({
     required this.id,
     this.text = '',
     this.note = '',
     this.parentId,
-    this.type = LifeTreeNodeType.branch,
   });
 
   LifeTreeNodeData copyWith({
     String? text,
     String? note,
     String? parentId,
-    LifeTreeNodeType? type,
   }) {
     return LifeTreeNodeData(
       id: id,
       text: text ?? this.text,
       note: note ?? this.note,
       parentId: parentId ?? this.parentId,
-      type: type ?? this.type,
     );
   }
 
@@ -41,7 +32,6 @@ class LifeTreeNodeData extends Equatable {
       'text': text,
       'note': note,
       'parentId': parentId,
-      'type': type.name,
     };
   }
 
@@ -51,13 +41,9 @@ class LifeTreeNodeData extends Equatable {
       text: json['text'] as String? ?? '',
       note: json['note'] as String? ?? '',
       parentId: json['parentId'] as String?,
-      type: LifeTreeNodeType.values.firstWhere(
-        (e) => e.name == json['type'],
-        orElse: () => LifeTreeNodeType.branch,
-      ),
     );
   }
 
   @override
-  List<Object?> get props => [id, text, note, parentId, type];
+  List<Object?> get props => [id, text, note, parentId];
 }
